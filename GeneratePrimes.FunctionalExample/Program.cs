c8455a4be4504f5668cb1a93b2b5f0c7d28dd9c8
Andrew Kicks <andrewkicks@gmail.com>
2018-05-13 00:47:02 +0100

    Create Extension Methods and Application based on functional programming example in video https://www.youtube.com/watch?v=I2tMmsZC1ZU&index=12&list=PLHY-5V32RcHEXbsLLIxL_wr_hm7kRjUV2 (38 minutes in)

--- GeneratePrimes.FunctionalExample/Program.cs
+++ GeneratePrimes.FunctionalExample/Program.cs
@@ -4,12 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GeneratePrimes.FunctionalExample
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace GeneratePrimes.FunctionalExample {
+    class Program {
+        static int Inc(int i) => i + 1;
+        static int StartValue = 2;
+        static int NumberToGet = 20;
+        static void Main(string[] args) => 
+            Sieves(Iterate(Inc, StartValue))
+                .Take(NumberToGet)
+                .ForEach(Console.WriteLine)
+                .ForEach(x => Console.WriteLine("Press To Continue"))
+                .ForEach(x => Console.ReadLine())
+                .ToList();
+
+        static IEnumerable<int> Iterate(Func<int, int> it, int val) {
+            while (true) {
+                yield return val;
+                val = it(val);
+            }
+        }
+        static IEnumerable<int> Sieves(IEnumerable<int> ints) {
+            while (true) {
+                var p = ints.First();
+                yield return p;
+                ints = ints.Where(n => n % p != 0);
+            }
+        }
+
+    }
+    public static class Extension {
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> arr, Action<T> act) {
+            foreach (var item in arr) {
+                act(item);
+                yield return item;
+            }
         }
     }
 }
